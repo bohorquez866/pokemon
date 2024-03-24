9fe881ba1d9b4f89a767230bb81fb53731597c86
Jesus B <bohorquez866@gmail.com>
2024-03-24 00:11:07 -0400

    initial commit

---
+++ frontend/src/main.ts
@@ -0,0 +1,15 @@
+import PrimeVue from 'primevue/config'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import './assets/main.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(PrimeVue)
+app.use(router)
+
+app.mount('#app')
